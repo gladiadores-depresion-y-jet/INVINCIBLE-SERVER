d30e3435da7ca3684394d088841cfda2e63b54b0
David <david.azoherra@gmail.com>
2019-05-25 20:36:24 -0600

    Commit inicial incluyendo bibliotecas necesarias

---
+++ cmake-build-debug/CMakeFiles/Invincible_Server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Invincible_Server.dir/main.cpp.o"
+  "Invincible_Server.pdb"
+  "Invincible_Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Invincible_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <pistache/net.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
